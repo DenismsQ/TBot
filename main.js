f42e1b2321df72593746bc5aeab91b65b488e6c6
DenismsQ <112930037+DenismsQ@users.noreply.github.com>
2023-10-12 11:26:43 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login('MTE1Mjg2MjUzNjY5ODQyOTUwMQ.GJtz63.zedyXNGbXElmwauZwznXSxj8RaYpLPu2-HahKA');
